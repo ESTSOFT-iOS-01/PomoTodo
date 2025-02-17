c2c4627675308568a4eda720e33c6cbd3add2c2a
shinseungjae <mok05247@naver.com>
2025-02-17 21:19:26 +0900

    프로젝트 초기 설정

---
+++ PomoTodo/PomoTodo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PomoTodo
+//
+//  Created by 신승재 on 2/17/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PomoTodo/PomoTodo/PomoTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomoTodoApp.swift
+//  PomoTodo
+//
+//  Created by 신승재 on 2/17/25.
+//
+
+import SwiftUI
+
+@main
+struct PomoTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
