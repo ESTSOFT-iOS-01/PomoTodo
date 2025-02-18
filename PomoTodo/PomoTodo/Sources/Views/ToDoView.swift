9d91aa19b3af78e4b7acb260a55b87f43aba9891
심근웅 <geunwoong.sim@gmail.com>
2025-02-18 22:25:49 +0900

    간단한 로우파일 리스트파일로 합치기

--- PomoTodo/PomoTodo/Sources/Views/ToDoView.swift
+++ PomoTodo/PomoTodo/Sources/Views/ToDoView.swift
@@ -105,6 +105,25 @@ fileprivate struct HeaderView: View {
   }
 }
 
+fileprivate struct TodoRow: View {
+  @State var isCompleted: Bool = false
+  @State var name: String = "확인용 테스트 문자열입니다"
+  var color: Color
+  var body: some View {
+    HStack {
+      Button {
+        isCompleted.toggle()
+      } label: {
+        Label("", systemImage: isCompleted ? "checkmark.circle" : "circle")
+      }.foregroundStyle(color)
+      
+      TextField("내용을 입력해주세요", text: $name)
+        .font(.system(size: 17))
+        .strikethrough(isCompleted ? true : false)
+        .disableAutocorrection(true)
+    }
+  }
+}
 
 #Preview {
   ToDoView()
