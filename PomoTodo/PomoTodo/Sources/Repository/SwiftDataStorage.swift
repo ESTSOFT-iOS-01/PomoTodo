7977cc4409ae56054d7701ca18261d0a87f43793
shinseungjae <mok05247@naver.com>
2025-02-18 14:51:26 +0900

    [#14] SwiftDataStorage 구현

---
+++ PomoTodo/PomoTodo/Sources/Repository/SwiftDataStorage.swift
@@ -0,0 +1,45 @@
+//
+//  SwiftDataStorage.swift
+//  PomoTodo
+//
+//  Created by 신승재 on 2/18/25.
+//
+
+import Foundation
+import SwiftData
+
+final class SwiftDataStorage {
+  let modelContext: ModelContext = {
+    let schema = Schema([PomoDayDTO.self, AppConfigDTO.self])
+    let configuration = ModelConfiguration(
+      isStoredInMemoryOnly: false
+    )
+    
+    do {
+      let container = try ModelContainer(
+        for: schema,
+        configurations: [configuration]
+      )
+      return ModelContext(container)
+    } catch {
+      fatalError("ModelContext Error: \(error.localizedDescription)")
+    }
+  }()
+}
+
+enum SwiftDataError: LocalizedError {
+  case fetchError
+  case deleteError
+  case modelNotFound
+  
+  var errorDescription: String {
+    switch self {
+    case .fetchError:
+      "Fetch error"
+    case .deleteError:
+      "Delete error"
+    case .modelNotFound:
+      "Model not found"
+    }
+  }
+}
